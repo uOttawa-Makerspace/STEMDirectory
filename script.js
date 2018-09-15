b4348c1e460c679af5a8a8731ebab2cf0be68112
Nicolas Gnyra <nicolas@gnyra.com>
2018-09-15 04:50:58 -0400

    Add news marquee

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    // navigation buttons
     $('.location-btn').on('click', function () {
         var targetName = $(this).data('target');
 
@@ -7,5 +8,15 @@ $(document).ready(function () {
         $('#' + targetName + '-path').css('display', 'block');
         $('#' + targetName + '-bg').css('display', 'block');
         $('#' + targetName + '-text').css('display', 'block');
+    });
+
+    // news marquee
+    $.get('assets/data/news.json').done(function (data) {
+        var $news = $('#news');
+        var star = "\240\240\240\240\240\u2605\240\240\240\240\240";
+        var news = data["news"].join(star);
+
+        $news.append(star + news + star);
+        $news.marquee({duration: 15000});
     });
 });
